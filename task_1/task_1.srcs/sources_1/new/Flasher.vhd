7a15d8506571e8483bf9ba8d91bcb1b13bfa0d1e
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-10-23 14:31:46 +0200

    Flasher: Add implementation

--- task_1/task_1.srcs/sources_1/new/Flasher.vhd
+++ task_1/task_1.srcs/sources_1/new/Flasher.vhd
@@ -1,13 +1,25 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+-- Toggles the LED state every half second
+
 entity Flasher is
-    Port ( );
-end Flasher;
+	port (
+		LED : out std_logic_vector(3 downto 0);
+		CLK_66MHZ : in std_ulogic
+	);
+end;
 
-architecture Behavioral of Flasher is
-
+architecture FlasherArchitecture of Flasher is
+	signal counter : unsigned range 0 to 67108864;
 begin
+LED <= std_logic_vector(counter)(25);
 
+counterProcess : process(CLK_66MHZ)
+begin
+	if(rising_edge(CLK_66MHZ)) then
+		counter <= counter + 1;
+	end if;
+end process;
 
-end Behavioral;
+end FlasherArchitecture;
