d091e7e42b4d76b3af48c3db1fb1876a601a7cb8
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-11-26 16:34:59 +0100

    add flying speghetti monster

---
+++ task_3/task_3.srcs/sim_1/new/FSM.vhd
@@ -0,0 +1,105 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/26/2014 04:11:01 PM
+-- Design Name: 
+-- Module Name: FSM - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity FSM is
+    Port (
+		clk    : in std_logic;
+		out_val: in  std_logic_vector(7 downto 0);
+		
+		-- everything below is the interface to the i2c driver
+    	start,
+		stop,
+		read,
+		write,
+		ack_in : out std_logic;
+		din    : out std_logic_vector(7 downto 0);
+		dout   : in  std_logic_vector(7 downto 0);
+		
+		cmd_ack  : in std_logic; -- command done
+		ack_out  : in std_logic
+    );
+end FSM;
+
+architecture moore of FSM is
+	type StateType is (S1, S2, S3, S4);
+	signal currentState, nextState : StateType;
+
+begin
+	transition : process(currentState, cmd_ack)
+	begin
+		if cmd_ack = '1' then
+			case currentState is 
+				when S1 =>
+					nextState <= S2;
+				when S2 =>
+					nextState <= S3;
+				when S3 =>
+					nextState <= S4;
+				when S4 =>
+					nextState <= S1;
+			end case;
+		end if;
+	end process;
+	
+	stateMemory : process(clk)
+	begin
+		if rising_edge(clk) then
+			currentState <= nextState;
+		end if;
+	end process;
+	
+	outputs : process(currentState)
+	begin
+		case currentState is 
+			when S1 =>
+				start <= '1';
+				stop <= '0';
+				ack_in <= '0';
+				din <= "10010000";
+			when S2 =>
+				stop <= '1';
+				start <= '0';
+				din <= "100011XX";
+			when S3 =>
+				start <= '1';
+				stop <= '0';
+				din <= "10010001";
+			when S4 =>
+				stop <= '1';
+				start <= '0';
+				out_val <= dout;
+				ack_in <= '1';
+		end case;
+	end process;
+
+end moore;
