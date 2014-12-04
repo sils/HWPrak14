12ce28b7ae6bdcf93fe2f8564d1660fe3392decc
unknown <sims1301@W02P52.tu-harburg.de>
2014-12-04 13:51:42 +0100

    add master_fpga

---
+++ task_3/task_3.srcs/sources_1/new/master_fpga.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12/04/2014 01:44:52 PM
+-- Design Name: 
+-- Module Name: master_fpga - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity master_fpga is
+    Port ( );
+end master_fpga;
+
+architecture Behavioral of master_fpga is
+
+begin
+
+
+end Behavioral;
